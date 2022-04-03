d438333d98775c0133b903deccc2a9a4e2d6ac67
quacamole <102833311+quacamole@users.noreply.github.com>
2022-04-02 20:19:45 -0700

    added assets

---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class ExitOnClick : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
